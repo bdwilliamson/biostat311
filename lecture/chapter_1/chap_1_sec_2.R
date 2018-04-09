c076f67120cc9b0bc24e241500305db5c0cc2d0c
Brian Williamson <brianw26@uw.edu>
2018-04-09 10:58:59 -0700

    added EM plots, slides

--- lecture/chapter_1/chap_1_sec_2.R
+++ lecture/chapter_1/chap_1_sec_2.R
@@ -25,6 +25,22 @@ data_func_confound <- function(beta0, beta1, beta2, n) {
 ## data generating mechanism for two predictors, one is a precision variable
 
 ## data generating mechanism for two predictors, one is an effect modifier
+data_func_em <- function(beta0, beta1, beta2, n) {
+  ## generate some X data: X1 is continuous, X2 is binary
+  x2 <- sample(0:1, n, replace = TRUE)
+  ## generate two clouds of x1 points
+  x1_0 <- runif(sum(x2 == 0), 0, 0.7)
+  x1_1 <- runif(sum(x2 == 1), 0.3, 1)
+  X <- matrix(NA, ncol = 2, nrow = n)
+  X[x2 == 0, 1] <- x1_0
+  X[x2 == 1, 1] <- x1_1
+  X[, 2] <- x2
+  
+  ## create the outcome
+  y <- beta0 + (beta1 + beta2*X[, 2])*X[, 1] + rnorm(n, 0, 1)
+  
+  return(data.frame(x1 = X[, 1], x2 = X[, 2], y = y))
+}
 
 beta0 <- 6
 beta1 <- -2
@@ -79,4 +95,73 @@ legend("bottomright", legend = c(expression(paste(X[2],  " = 0", sep = "")), exp
 lines(x_range[, 2], x_range%*%mod_c_simple$coefficients)
 lines(x_range_0[, 2], x_range_0%*%mod_c_multiple$coefficients, col = "red")
 lines(x_range_1[, 2], x_range_1%*%mod_c_multiple$coefficients, col = "blue")
+dev.off()
+
+
+## -------------------
+## Effect modification
+## -------------------
+beta0_em <- 6
+beta1_em <- 2
+beta2_em <- 3
+
+set.seed(4747)
+dat_e <- data_func_em(beta0_em, beta1_em, beta2_em, n = 100)
+
+## make a plot, without coloring
+png("lecture/chapter_1/plots/effect_modification_simple.png")
+plot(dat_e$x1, dat_e$y, main = "", xlab = expression(X[1]), ylab = "Y", pch = 16,
+     xlim = c(0, 1), ylim = c(0, max(dat_e$y) + 0.5))
+dev.off()
+
+## fit best fitting line
+mod_e <- lm(y ~ x1, data = dat_e)
+
+## plot with best fitting line
+png("lecture/chapter_1/plots/effect_modification_simple_with_line.png")
+plot(dat_e$x1, dat_e$y, main = "", xlab = expression(X[1]), ylab = "Y", pch = 16,
+     xlim = c(0, 1), ylim = c(0, max(dat_e$y) + 0.5))
+lines(x_range[, 2], x_range%*%mod_e$coefficients)
+text(0.2, 11, labels = paste0("estimated slope = ", round(mod_e$coefficients[2], 3)))
+dev.off()
+
+## plot without lines, but colored by x2 status
+png("lecture/chapter_1/plots/effect_modification_colored.png")
+plot(dat_e$x1, dat_e$y, main = "", xlab = expression(X[1]), ylab = "Y", 
+     pch = ifelse(dat_e$x2 == 1, 17, 16),
+     xlim = c(0, 1), ylim = c(0, max(dat_e$y) + 0.5),
+     col = ifelse(dat_e$x2 == 1, "blue", "red"))
+legend("bottomright", legend = c(expression(paste(X[2],  " = 0", sep = "")), expression(paste(X[2], " = 1", sep = ""))),
+                             pch = c(16, 17), col = c("red", "blue"))
+dev.off()
+
+## fit multiple regression with em
+mod_e_multiple <- lm(y ~ x1*x2, data = dat_e)
+
+## with simple line
+png("lecture/chapter_1/plots/effect_modification_colored_with_simple_line.png")
+plot(dat_e$x1, dat_e$y, main = "", xlab = expression(X[1]), ylab = "Y", 
+     pch = ifelse(dat_e$x2 == 1, 17, 16),
+     xlim = c(0, 1), ylim = c(0, max(dat_e$y) + 0.5),
+     col = ifelse(dat_e$x2 == 1, "blue", "red"))
+lines(x_range[, 2], x_range%*%mod_e$coefficients)
+legend("bottomright", legend = c(expression(paste(X[2],  " = 0", sep = "")), expression(paste(X[2], " = 1", sep = ""))),
+       pch = c(16, 17), col = c("red", "blue"))
+dev.off()
+
+
+x_range_0_em <- cbind(x_range[1:71, ], 0, 0)
+x_range_1_em <- cbind(x_range[31:101, ], 1, x_range[31:101, 2])
+
+## with multiple lines
+png("lecture/chapter_1/plots/effect_modification_colored_with_lines.png")
+plot(dat_e$x1, dat_e$y, main = "", xlab = expression(X[1]), ylab = "Y", 
+     pch = ifelse(dat_e$x2 == 1, 17, 16),
+     xlim = c(0, 1), ylim = c(0, max(dat_e$y) + 0.5),
+     col = ifelse(dat_e$x2 == 1, "blue", "red"))
+lines(x_range[, 2], x_range%*%mod_e$coefficients)
+lines(x_range_0_em[, 2], x_range_0_em%*%mod_e_multiple$coefficients, col = "red")
+lines(x_range_1_em[, 2], x_range_1_em%*%mod_e_multiple$coefficients, col = "blue")
+legend("bottomright", legend = c(expression(paste(X[2],  " = 0", sep = "")), expression(paste(X[2], " = 1", sep = ""))),
+       pch = c(16, 17), col = c("red", "blue"))
 dev.off()
